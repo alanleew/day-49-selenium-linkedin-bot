23cddfdac3f0ca55ce730cf2847be115de7e03b7
alanleew <alanleew412@gmail.com>
2025-03-28 12:19:14 -0700

    First draft upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+
+LINKEDIN_URL = 'https://www.linkedin.com/jobs/search/?currentJobId=4195337096&f_AL=true&geoId=103644278&keywords=python%20developer&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true'
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+
+driver.get(LINKEDIN_URL)
+
+time.sleep(2)
+sign_in_button = driver.find_element(By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section/div/div/div/div[2]/button')
+sign_in_button.click()
+
+time.sleep(2)
+email_field = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_key"]')
+email_field.send_keys(EMAIL)
+password_field = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_password"]')
+password_field.send_keys(PASSWORD)
+
+time.sleep(2)
+next_sign_in_button = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal"]/div/section/div/div/form/div[2]/button')
+next_sign_in_button.click()
+
+driver.quit()
